4f912068e60f8c10ad399daba2faf65f04e403ad
Ivan Kurbasov <ivan.kurbasov@gmail.com>
2024-05-17 00:01:18 +0300

    1

---
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        System.out.println(123);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
